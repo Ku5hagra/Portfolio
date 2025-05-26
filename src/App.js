e7370f5f16ae21a697b1d98a3ef35f2870311db4
Kushagra Agrawal <kushagraa216@gmail.com>
2025-05-26 12:27:07 +0530

    loader

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import Loader from "./components/loader";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Footer from "./components/footer";
 import Navbar from "./components/navbar";
@@ -13,21 +14,37 @@ import Blogpage from "./components/blogpage";
 import SingleBlog from "./components/singleblog";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Show loader for 5 seconds
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+
+    // Cleanup timer
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <Provider>
-      <BrowserRouter>
-        <Navbar />
-        <Switch>
-          <Route exact path="/" component={homepage} />
-          <Route exact path="/certification" component={Certification} />
-          <Route exact path="/contact" component={ContactUs} />
-          <Route exact path="/project/:id" component={SingleProject} />
-          <Route exact path="/projectpage" component={Projectpage} />
-          <Route exact path="/blog/:id" component={SingleBlog} />
-          <Route exact path="/blogpage" component={Blogpage} />
-        </Switch>
-        <Footer />
-      </BrowserRouter>
+      {loading ? (
+        <Loader /> // Show loader if still loading
+      ) : (
+        <BrowserRouter>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Homepage} />
+            <Route exact path="/certification" component={Certification} />
+            <Route exact path="/contact" component={ContactUs} />
+            <Route exact path="/project/:id" component={SingleProject} />
+            <Route exact path="/projectpage" component={Projectpage} />
+            <Route exact path="/blog/:id" component={SingleBlog} />
+            <Route exact path="/blogpage" component={Blogpage} />
+          </Switch>
+          <Footer />
+        </BrowserRouter>
+      )}
     </Provider>
   );
 }
---
+++ src/components/Loader.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Loader.css"; // We'll create this CSS file for styling
+
+const Loader = () => {
+  return (
+    <div className="loader-container">
+      <div className="spinner"></div>
+      <h2 className="loader-text">Loading, please wait...</h2>
+    </div>
+  );
+};
+
+export default Loader;
