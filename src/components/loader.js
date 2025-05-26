0a3b0b356df8c2b440fc1e3d2b398a692d2b7fe4
Kushagra Agrawal <kushagraa216@gmail.com>
2025-05-26 20:17:52 +0530

    loader

---
+++ src/components/loader.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const Loader = () => {
+  return (
+    <div className="loader-container">
+      <div className="spinner"></div>
+      <h2 className="loader-text">Loading, please wait...</h2>
+    </div>
+  );
+};
+
+export default Loader;
